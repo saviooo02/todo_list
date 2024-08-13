ef6454eabb90adbb39782ebdd9f3883c175bd643
Savio Shaji <savioshaji02@gmail.com>
2024-08-13 20:06:00 +0530

    message

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 
 function Todo(){
 
-    const [tasks,setTasks]=useState(["Wakeup","Brush","Eat"])
+    const [tasks,setTasks]=useState([{text:"Wakeup",completed:false},
+                                    {text:"Brush",completed:false},
+                                    {text:"Eat",completed:false}])
+
     const [newTask,setNewTask] = useState("")
 
     const handleNewTask = (e)=>{
@@ -10,13 +13,25 @@ function Todo(){
     }
 
     const addTask = ()=>{
-        setTasks([...tasks,newTask])
+        setTasks([...tasks,{text:newTask,completed:false}])
+        setNewTask("")
     }
 
     const removeTask = (index)=>{
         const updatedTasks = tasks.filter((_,i)=>i !== index)
         setTasks(updatedTasks)
     }
+
+    const doneTask = (index) => {
+        const updatedTask = tasks.map((task, i) => {
+          if (i === index) {
+            return { ...task, completed: true }; // Create a new object with completed set to true
+          }
+          return task; // Return the original task if it's not the one being marked as done
+        });
+
+    setTasks(updatedTask)
+}
 
     return(
         <div>
@@ -26,7 +41,7 @@ function Todo(){
 
            <ol>
             {tasks.map((task,index)=>
-                <li key={index}>{task}<button onClick={()=>removeTask(index)}>Delete</button></li>
+                <li key={index} style={{textDecoration : task.completed ? "line-through" : "none"}}>{task.text}<button onClick={()=>doneTask(index)}>Done</button><button onClick={()=>removeTask(index)}>Delete</button></li>
             )}
            </ol>
         </div>
