fde59ce36619a60c227693346efe5f5bb259123a
Savio Shaji <savioshaji02@gmail.com>
2024-08-13 11:26:54 +0530

    Add and Delete btns

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Todo(){
+
+    const [tasks,setTasks]=useState(["Wakeup","Brush","Eat"])
+    const [newTask,setNewTask] = useState("")
+
+    const handleNewTask = (e)=>{
+        setNewTask(e.target.value);
+    }
+
+    const addTask = ()=>{
+        setTasks([...tasks,newTask])
+    }
+
+    const removeTask = (index)=>{
+        const updatedTasks = tasks.filter((_,i)=>i !== index)
+        setTasks(updatedTasks)
+    }
+
     return(
         <div>
-            <input type="checkbox"/>
+            <input type="text" value={newTask} onChange={handleNewTask} />
+            <button onClick={addTask}>Add</button>
+
+
+           <ol>
+            {tasks.map((task,index)=>
+                <li key={index}>{task}<button onClick={()=>removeTask(index)}>Delete</button></li>
+            )}
+           </ol>
         </div>
     )
 }
